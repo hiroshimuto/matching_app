ceeea2fb2bb163a7dfb83555b88689d4ee8bf89b
hiroshimuto <hiroshi.github@gmail.com>
2019-02-01 18:08:59 +0900

    create index action and view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,8 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: %i(name introduction sex img))
     devise_parameter_sanitizer.permit(:account_update, keys: %i(name introduction sex img))
   end
+
+  def after_sign_in_path_for(resource)
+    users_path
+  end
 end
